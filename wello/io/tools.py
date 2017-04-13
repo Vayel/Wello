02816fb9b9c50dc097c804702778b65203161c2a
Léo Milhade <leo.milhade@ensimag.grenoble-inp.fr>
2017-04-13 16:48:39 +0200

    add and display median filter

--- wello/io/tools.py
+++ wello/io/tools.py
@@ -1,7 +1,26 @@
+import scipy.signal
+
+from matplotlib import pyplot as plt
+
 from .. import models
 
 TOTAL_HEIGHT = 300
 
+last_distances = [0 for i in range(20)]
+last_distances_filtered = last_distances
 
 def distance_to_volume(distance):
+    # keep the last 20 distances in a list
+    del last_distances[0]
+    last_distances.append(TOTAL_HEIGHT - distance)
+    last_distances_filtered = scipy.signal.medfilt(last_distances)
+
+    print()
+    print("Original : ", last_distances)
+    print("Filtered : ", [int(i) for i in last_distances_filtered])
+    
+    plt.plot(last_distances)
+    plt.plot(last_distances_filtered)
+    plt.show()
+
     return TOTAL_HEIGHT - distance
