f87094983350a02b226097287f9848f54032b24a
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-05-11 17:21:46 +0200

    Try moving average

--- filter_volume.py
+++ filter_volume.py
@@ -2,17 +2,33 @@ import csv
 from datetime import datetime
 
 from scipy.signal import lfilter
+import numpy as np
 import matplotlib.pyplot as plt 
 
 
-x = []
-y = []
+def read_data():
+    x = []
+    y = []
 
-with open('volumes.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        x.append(datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f'))
-        y.append(int(row[1]))
+    with open('volumes.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            x.append(datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f'))
+            y.append(int(row[1]))
 
-plt.plot(x, y)
-plt.show()
+    return x, y
+
+
+def movingaverage(interval, window_size):
+    window = np.ones(int(window_size))/float(window_size)
+    return np.convolve(interval, window, 'same')
+
+
+if __name__ == '__main__':
+    x, y = read_data()
+    plt.plot(x, y)
+
+    y_av = movingaverage(y, 5)
+    plt.plot(x, y_av, "r")
+
+    plt.show()
