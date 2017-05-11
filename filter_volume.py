6648ee7bcc43d5dd52f804aa9c46385ee8d1a325
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-05-11 17:32:07 +0200

    Try low filter

--- filter_volume.py
+++ filter_volume.py
@@ -1,7 +1,7 @@
 import csv
 from datetime import datetime
 
-from scipy.signal import lfilter
+from scipy.signal import lfilter, butter
 import numpy as np
 import matplotlib.pyplot as plt 
 
@@ -24,11 +24,31 @@ def movingaverage(interval, window_size):
     return np.convolve(interval, window, 'same')
 
 
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
 if __name__ == '__main__':
     x, y = read_data()
-    plt.plot(x, y)
+    plt.plot(x, y, label='Original')
 
     y_av = movingaverage(y, 5)
-    plt.plot(x, y_av, "r")
+    plt.plot(x, y_av, "r", label='MA')
+
+    order = 6
+    fs = 30.0       # sample rate, Hz
+    cutoff = 3.667  # desired cutoff frequency of the filter, Hz
+
+    y_filter = butter_lowpass_filter(y, cutoff, fs, order)
+    plt.plot(x, y_filter, "g", label='Filter')
 
     plt.show()
